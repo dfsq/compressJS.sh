f080345b4c2a19372b039c2da0b8fabf11558fdb
dfsq.dfsq@gmail.com <dfsq.dfsq@gmail.com>
2011-09-16 18:26:32 +0300

    Error fixed.

--- compressJS.sh
+++ compressJS.sh
@@ -8,13 +8,13 @@ NEWFILE="build${DATEFORMAT}.js";
 # Itearate through all files
 for f in $*
 do
-	js_code="${code} --data-urlencode js_code@${f}"
+	code="${code} --data-urlencode js_code@${f}"
 done
 
 `curl \
 --url ${API_URL} \
 --header 'Content-type: application/x-www-form-urlencoded' \
-${js_code} \
+${code} \
 --data output_format=text \
 --data output_info=compiled_code \
 --data compilation_level=${COMPILATION_LEVEL} \
