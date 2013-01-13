05a46017cd7608dc2457305e46e57cde20242998
Aliaksandr Astashenkau <dfsq.dfsq@gmail.com>
2013-01-14 00:44:04 +0300

    - Real file names in error reports  - bash instead of sh  - Little clean up.

--- compressjs.sh
+++ compressjs.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # Constants
 SERVICE_URL=http://closure-compiler.appspot.com/compile
-NEWFILE="build-`date +"%d%m%y"`.js"
+NEWFILE="c`date +"%d%m%y"`.js"
 
 # Check if files to compile are provided
 if [ $# -eq 0 ]
@@ -34,29 +34,29 @@ ${code} \
 --data output_info=errors \
 --data compilation_level=SIMPLE_OPTIMIZATIONS |
 
-# echo '{"compiledCode":"var a\u003d51,b\u003d0.4\u003cMath.random();a\u0026\u0026b\u0026\u0026alert(\"Test record\");","statistics":{"originalSize":80,"originalGzipSize":97,"compressedSize":56,"compressedGzipSize":76,"compileTime":0}}' |
-# echo '{"compiledCode":"","errors":[{"type":"JSC_PARSE_ERROR","file":"Input_1","lineno":1,"charno":32,"error":"Parse error. missing ) after argument list","line":"alert('This script consists of' error);"}],"statistics":{"originalSize":121,"originalGzipSize":126,"compressedSize":0,"compressedGzipSize":20,"compileTime":0}}' |
-
 python -c '
-import json, sys 
-data = json.load(sys.stdin) 
+import json, sys
+data = json.load(sys.stdin)
+
 if "errors" in data:
-	print "========================================\n COMPILATION FAILED WITH ERRORS \n========================================"
+	print "### COMPILATION FAILED WITH ERRORS"
 	for err in data["errors"]:
-		print "File: %s, %d:%d" % (err["file"], err["lineno"], err["charno"])
+		file = sys.argv[int(err["file"].replace("Input_", "")) + 1]
+		print "File: %s, %d:%d" % (file, err["lineno"], err["charno"])
 		print "Error: %s" % err["error"]
 		print "Line: %s" % err["line"]
+		
+	print "\nBuild failed.\n"
+	
 else:
-	print "========================================\n COMPILATION COMPLETED \n========================================"
+	print "### COMPILATION COMPLETED"
 	print "Original size: %db, gziped: %db" % (data["statistics"]["originalSize"], data["statistics"]["originalGzipSize"])
 	print "Compressed size: %db, gziped: %db" % (data["statistics"]["compressedSize"], data["statistics"]["compressedGzipSize"])
 	print "Compression rate: %.2f" % (float(data["statistics"]["compressedSize"]) / int(data["statistics"]["originalSize"]))
 
-	filename = "'$NEWFILE'"
+	filename = "'${NEWFILE}'"
 	f = open(filename, "w")
 	f.write(data["compiledCode"])
 
-	print "\nBuild file %s created." % filename
-'
-
-echo "\n"
+	print "\nBuild file %s created.\n" % filename
+' $@
